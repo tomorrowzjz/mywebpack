37ef06ef5ebb4e3ae0cd233cc15be05c199a281e
tomorrowzjz <1047547064@qq.com>
2018-11-20 12:27:29 +0800

    '注释'

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   output:{
     path:path.resolve(__dirname,'dist'),
-    filename:'[name]-bundle.js'
+    //filename前面我们可以使用一个变量[name],这个就表示获取entry里面的key作为文件名加在前面
+    filename:'[name]-bundle[chunkhash:8].js'
   },
   plugins:[
     new HtmlWebpackPlugin({
@@ -20,16 +21,16 @@ module.exports = {
       filename:'index.html',
       template:'./src/index.html',
       title:'haha',
-      hash:true,
+      hash:true,//hash为true的配置,它就会自动在引入的src地址后面增加一段hash值
       minify:{
         collapseWhitespace:true //折叠空白区域 也就是压缩代码
       }
     }),
     new HtmlWebpackPlugin({
       chunks:['index2'],
-      filename:'index1.html',
-      template:'./src/index2.html',
-      title:'haha2',
+      filename:'index1.html',//指定生成的html名称
+      template:'./src/index2.html',//调用模板名称
+      title:'haha2',//模板的title
       hash:true,
       minify:{
         collapseWhitespace:true //折叠空白区域 也就是压缩代码
